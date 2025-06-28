5e3998c9f609b92db2087a7e831a43970ce43b04
hkw2304 <hkw2304@gmail.com>
2025-06-28 15:19:10 +0900

    this is push

--- lib/test.dart
+++ lib/test.dart
@@ -9,6 +9,8 @@ class Samples extends StatefulWidget {
 class _SamplesState extends State<Samples> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Text("helo"),
+    );
   }
 }
