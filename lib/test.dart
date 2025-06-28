e222a51729d5180a37871f7ac1954b420c4b3b43
hkw2304 <hkw2304@gmail.com>
2025-06-28 15:17:50 +0900

    test addgit add lib/test.dart!

--- lib/test.dart
+++ lib/test.dart
@@ -9,6 +9,6 @@ class Samples extends StatefulWidget {
 class _SamplesState extends State<Samples> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
