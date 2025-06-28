1005329473becd632e1877f33003d20507218f2d
hkw2304 <hkw2304@gmail.com>
2025-06-28 11:03:04 +0900

    dev change

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '다시 한번'),
+      home: const MyHomePage(title: 'dev에서 변경'),
     );
   }
 }
