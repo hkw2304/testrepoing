d7d521b3a28b988163d1fea9379dca59f944d11b
hkw2304 <hkw2304@gmail.com>
2025-06-28 15:10:11 +0900

    only test.dart add

---
+++ lib/test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Sample extends StatefulWidget {
+  const Sample({super.key});
+
+  @override
+  State<Sample> createState() => _SampleState();
+}
+
+class _SampleState extends State<Sample> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
